538201c1a7e6ebc557eca3382b991c9c2dc0984d
Ylia639 <yliaivanova003@gmail.com>
2024-09-16 16:12:37 +0300

    Update 023.cpp

--- 023.cpp
+++ 023.cpp
@@ -29,3 +29,66 @@ int main()
 
 }
 
+#include <iostream>
+
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	if (a >= 100'000 || b >= 100'000 || c >= 100'000)
+	{
+
+		std::cout << "error\n";
+		return 0;
+	}
+	if(a<0 || b<0 || c<0 )
+	{
+
+		std::cout << "error\n";
+		return 0;
+	}
+
+	std::cout << "a = " << a << "\n";
+	std::cout << "b = " << b << "\n";
+	std::cout << "c = " << c << "\n";
+
+	if (a >= b && b >= c)
+	{
+		std::cout << a - c << "\n";
+		return 0;
+	}
+	if (b >= a && a >= c)
+	{
+		std::cout << b - c << "\n";
+		return 0;
+	}
+	if (c >= b && b >= a)
+	{
+		std::cout << c - a << "\n";
+		return 0;
+	}
+	if (c >= a && a >= b)
+	{
+		std::cout << c - b << "\n";
+		return 0;
+	}
+	if (b >= c && c >= a)
+	{
+		std::cout << b - a << "\n";
+		return 0;
+	}
+	if (a>=c && c>=b)
+	{
+		std::cout << a - b << "\n";
+		return 0;
+	}
+	if (c>=a && a>=b)
+	{
+		std::cout << c - b << "\n";
+		return 0;
+	}
+
+
+}
